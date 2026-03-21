ec7b4cd5c4a25b6ad7f482a6928d27beffa15816
tchering <sonam.sherpa.lama@gmail.com>
2026-03-21 09:23:26 +0100

    Initial Commit

---
+++ 15CMLP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  15CMLP
+//
+//  Created by sonam sherpa on 21/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 15CMLP/_5CMLPApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5CMLPApp.swift
+//  15CMLP
+//
+//  Created by sonam sherpa on 21/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct _5CMLPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
